8955b5df712ad12e14149862463920c4efc28572
rachwood1111 <rachwood1111@gmail.com>
2025-11-19 21:38:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-/// <reference types="node" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+// Manually declare process to avoid depending on @types/node during build
+declare const process: { env: Record<string, string | undefined> };
 
 // https://vitejs.dev/config/
 export default defineConfig({
